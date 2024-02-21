c8e72a97c0c93ff325820c8ff3c0648c64561567
zeeshanmaqbool200 <110154401+zeeshanmaqbool200@users.noreply.github.com>
2024-02-21 14:04:33 +0530

    Add files via upload

--- books.js
+++ books.js
@@ -5,6 +5,87 @@ const authorHero = document.getElementById('author');
 const detailsHero = document.getElementById('details-hero');
 const cartContainer = document.getElementById('container');
 const boxes = document.querySelectorAll('.image');
+//Boxes Click
+boxes.forEach(box=>{
+  box.addEventListener('click', ()=>{
+    let boxImg = box.querySelector("img");
+    let boxHead = box.querySelector("#text h3");
+    let boxText = box.querySelector("#text p");
+    let cartHeader = cartContainer.querySelector('#header');
+    let cartDescription = cartContainer.querySelector('#description');
+    let btnText = document.getElementById('btn-text');
+    let cartImage = cartContainer.querySelector('#left-image');
+    cartHeader.innerText = boxHead.innerText;
+    cartImage.innerHTML = boxImg.outerHTML;
+    cartDescription.innerText = boxText.innerText;
+    btnText.innerHTML = `<a href="https://wa.me/+916005156100?text=Salamun Alikum I Want *${cartHeader.innerText}* book of *_${cartDescription.innerText}_* from your website https://idarahwaliulasr.netlify.app/library .Let me know when and how this would be available.">Order</a>`
+    cartContainer.style.display = "grid";
+    document.body.style.overflow = "hidden";
+    
+  })
+});
+function hideCart() {
+  cartContainer.style.display = "none";
+ document.body.style.overflow = "auto";
+    
+}
+
+// Hero Random
+window.addEventListener('DOMContentLoaded', randomBookFunc)
+
+function randomBookFunc() {
+  let randomBook = Math.floor(Math.random() * boxes.length);
+  
+  popularBook.innerHTML = boxes[randomBook].outerHTML;
+
+}
+
+//Search Start 
+
+function resetQuery() {
+  search.value = "";
+  searchQuery();
+  searchTextOutput.innerText = "";
+}
+
+
+let searchTimeout;
+
+function searchQuery() {
+  // Clear the previous timeout
+  clearTimeout(searchTimeout);
+
+  // Set a new timeout for 1000 milliseconds (1 second)
+  searchTimeout = setTimeout(function () {
+    searchTerm = search.value.toUpperCase();
+
+    boxes.forEach(title => {
+      text = title.textContent.toUpperCase();
+
+      if (text.includes(searchTerm)) {
+        title.style.display = "flex";
+        if (search.value == null || search.value.length == 0) {
+          searchTextOutput.innerText = "Enter Any  Query";
+        } else{
+        searchTextOutput.innerText = ` Results for " ${search.value}"`;
+          
+        }
+      } else {
+        title.style.display = "none";
+    
+      }
+    });
+  }, 1000);
+}
+
+// Assuming you have a button with the ID 'searchButton'
+const searchButton = document.getElementById('searchButton');
+const resetButton = document.getElementById('resetButton');
+
+// Add a click event listener to the button
+resetButton.addEventListener('click', resetQuery);
+searchButton.addEventListener('click', searchQuery);
+/*
 //Nav Start
 (function($) {
   $(function() {
@@ -38,67 +119,4 @@ const boxes = document.querySelectorAll('.image');
   });
 })(jQuery);
 //Nav End
-//Boxes Click
-boxes.forEach(box=>{
-  box.addEventListener('click', ()=>{
-    let boxImg = box.querySelector("img");
-    let boxHead = box.querySelector("#text h3");
-    let boxText = box.querySelector("#text p");
-    let cartHeader = cartContainer.querySelector('#header');
-    let cartDescription = cartContainer.querySelector('#description');
-    let cartImage = cartContainer.querySelector('#left-image');
-    cartHeader.innerText = boxHead.innerText;
-    cartImage.innerHTML = boxImg.outerHTML;
-    cartDescription.innerText = boxText.innerText;
-    cartContainer.style.display = "grid";
-    document.body.style.overflow = "hidden";
-    
-  })
-});
-function hideCart() {
-  cartContainer.style.display = "none";
- document.body.style.overflow = "auto";
-    
-}
-
-// Hero Random
-window.addEventListener('DOMContentLoaded', randomBookFunc)
-
-function randomBookFunc() {
-  let randomBook = Math.floor(Math.random() * boxes.length);
-  
-  popularBook.innerHTML = boxes[randomBook].outerHTML;
-
-}
-
-//Search Start 
-
-let searchTimeout;
-
-function searchQuery() {
-  // Clear the previous timeout
-  clearTimeout(searchTimeout);
-
-  // Set a new timeout for 1000 milliseconds (1 second)
-  searchTimeout = setTimeout(function () {
-    searchTerm = search.value.toUpperCase();
-
-    boxes.forEach(title => {
-      text = title.textContent.toUpperCase();
-
-      if (text.includes(searchTerm)) {
-        title.style.display = "flex";
-        searchTextOutput.innerText = ` Results for " ${search.value}"`
-      } else {
-        title.style.display = "none";
-    
-      }
-    });
-  }, 1000);
-}
-
-// Assuming you have a button with the ID 'searchButton'
-const searchButton = document.getElementById('searchButton');
-
-// Add a click event listener to the button
-searchButton.addEventListener('click', searchQuery);
+*/
